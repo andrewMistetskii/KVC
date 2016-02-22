83b5bc993e9608e0864b0b1fa8770544874c8939
Zetokz <BitbucketAndroid>
2016-02-22 16:51:36 +0200

    first

---
+++ KVC/Student.h
@@ -0,0 +1,15 @@
+//
+//  Student.h
+//  KVC
+//
+//  Created by Андрей Мистецкий on 2/22/16.
+//  Copyright © 2016 Андрей Мистецкий. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@property (strong, nonatomic) NSString *name;
+
+@end
---
+++ KVC/Student.m
@@ -0,0 +1,16 @@
+//
+//  Student.m
+//  KVC
+//
+//  Created by Андрей Мистецкий on 2/22/16.
+//  Copyright © 2016 Андрей Мистецкий. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+
+
+
+@end
